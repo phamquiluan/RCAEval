63eeaa3f02897070e3ef10d539d25b784dcf557d
Luan Pham <phamquiluan@gmail.com>
2024-08-11 04:17:08 +0000

    Add fci

---
+++ RCAEval/graph_construction/fci.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from causallearn.search.ConstraintBased.FCI import fci
+
+
+def fci_default(data: pd.DataFrame):
+    node_names = data.columns.to_list()
+
+    # fill nan by ffill
+    data = data.fillna(method="ffill")
+
+    data = data.to_numpy().astype(float)
+
+    output = fci(data, node_names=node_names)
+    adj = output[0].graph
+    return adj
