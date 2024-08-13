3ac224fa2dc678ab55339fb69cffcc324bd28a78
Luan Pham <phamquiluan@gmail.com>
2024-08-13 04:42:53 +0000

    Add auto download for online-boutique dataset

--- RCAEval/utility/__init__.py
+++ RCAEval/utility/__init__.py
@@ -1,5 +1,10 @@
 import json
 import sys
+import os
+from os.path import join
+import requests
+import zipfile
+from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
@@ -50,3 +55,45 @@ def convert_adjacency_matrix(adj, node_names):
         service_adj[i][i] = 0
 
     return service_adj, services  # services is node_names but for services
+
+
+def download_data(remote_url=None, local_path=None):
+    """Download data from a remote URL."""
+    if remote_url is None:
+        remote_url = "https://github.com/phamquiluan/baro/releases/download/0.0.4/simple_data.csv"
+    if local_path is None:
+        local_path = "data.csv"
+
+    response = requests.get(remote_url, stream=True)
+    total_size_in_bytes = int(response.headers.get("content-length", 0))
+    block_size = 1024 # 1 Kibibyte
+
+    progress_bar = tqdm(
+        desc=f"Downloading {local_path}..",
+        total=total_size_in_bytes,
+        unit="iB",
+        unit_scale=True,
+    )
+
+    with open(local_path, "wb") as ref:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            ref.write(data)
+
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
+
+
+def download_online_boutique_dataset(local_path=None):
+    """Download the Online Boutique dataset from Zenodo."""
+    if local_path == None:
+        local_path = "data"
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+    if os.path.exists(join(local_path, "online-boutique")):
+        return
+    download_data("https://zenodo.org/records/13305663/files/online-boutique.zip?download=1", "online-boutique.zip")
+    with zipfile.ZipFile("online-boutique.zip", 'r') as file:
+        file.extractall(local_path)
+    os.remove("online-boutique.zip")
