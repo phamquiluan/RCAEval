36e53ff5a865771a90fc1743fb937426e9100162
Luan Pham <phamquiluan@gmail.com>
2024-08-21 12:42:29 +0000

    Add link.sh

---
+++ script/link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eux
+ln -fs "$PWD/lib/pyAgrum/lib/image.py" "$PWD/env-rcd/lib/python3.8/site-packages/pyAgrum/lib/"
+ln -fs "$PWD/lib/causallearn/search/ConstraintBased/FCI.py" "$PWD/env-rcd/lib/python3.8/site-packages/causallearn/search/ConstraintBased/"
+ln -fs "$PWD/lib/causallearn/utils/Fas.py" "$PWD/env-rcd/lib/python3.8/site-packages/causallearn/utils/"
+ln -fs "$PWD/lib/causallearn/utils/PCUtils/SkeletonDiscovery.py" "$PWD/env-rcd/lib/python3.8/site-packages/causallearn/utils/PCUtils/"
+ln -fs "$PWD/lib/causallearn/graph/GraphClass.py" "$PWD/env-rcd/lib/python3.8/site-packages/causallearn/graph/"
