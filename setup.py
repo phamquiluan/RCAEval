4199197a03c9bbb85b8deddf9faf276762573af7
Luan Pham <phamquiluan@gmail.com>
2024-08-24 06:25:46 +0000

    Bump version 0.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", encoding="utf-8") as f:
     
 setup(
     name="RCAEval",
-    version="0.1.1",
+    version="0.1.2",
     packages=["RCAEval"],
     include_package_data=True,
     long_description=long_description,
