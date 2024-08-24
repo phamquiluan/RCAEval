9458af8c390c5866fc3ec6b59cb097d8c3f4b591
Luan Pham <phamquiluan@gmail.com>
2024-08-24 06:00:21 +0000

    Add test basic functionality into test.py

--- tests/test.py
+++ tests/test.py
@@ -58,3 +58,21 @@ def test_rq1_script():
     # Check if the script ran successfully
     assert result.returncode == 0, f"Script failed with return code {result.returncode}\nOutput: {result.stdout}\nError: {result.stderr}"
     
+
+def test_basic():
+    # You can put the code here to a file named test.py
+    from RCAEval.e2e import baro
+    from RCAEval.utility import download_data, read_data
+
+    # download a sample data to data.csv
+    download_data()
+
+    # read data from data.csv
+    data = read_data("data.csv")
+    anomaly_detected_timestamp = 1692569339
+
+    # perform root cause analysis
+    root_causes = baro(data, anomaly_detected_timestamp)["ranks"]
+
+    # print the top 5 root causes
+    print("Top 5 root causes:", root_causes[:5])
