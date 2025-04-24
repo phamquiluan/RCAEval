9ab791e507c73185676f7f5e702a871ed1e0f6b4
Luan Pham <phamquiluan@gmail.com>
2025-04-24 14:59:23 +0000

    Bump v1.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", encoding="utf-8") as f:
     
 setup(
     name="RCAEval",
-    version="1.1.1",
+    version="1.1.2",
     packages=["RCAEval"],
     include_package_data=True,
     description="RCAEval: A Benchmark for Root Cause Analysis of Microservice Systems",
